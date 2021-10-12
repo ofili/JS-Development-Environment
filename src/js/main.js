00bbd89746c5b7b308efd9de88b40f3272954ce4
Lewis <ofili@users.noreply.github.com>
2021-10-12 10:38:54 +0100

    Bundling

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import '../css/main.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this course`);
