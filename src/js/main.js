4fd5bb075e40fdeeed50aafef190c950b61f4e60
Lewis <ofili@users.noreply.github.com>
2021-10-13 05:25:32 +0100

    HTTP Calls

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
-import '../css/main.css';
+import {getUsers} from '../api/users_api';
 
-import numeral from 'numeral';
+//Populate table of users via API call
+getUsers().then(result => {
+  let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-debugger; // eslint-disable-line no-debugger
-console.log(`I would pay ${courseValue} for this course`); // eslint-disable-line no-console
+  result.forEach(user => {
+    usersBody+= `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td> ${user.lastName}</td>
+    <td> ${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
